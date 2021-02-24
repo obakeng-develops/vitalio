ce8638e5765501e194aa4755b5c3cdcd6eb74c8e
Obakeng <obakeng@staffdeals.co.za>
2021-02-24 19:10:10 +0200

    Made changes to calls to env variables in Member and Provider Apps

--- vitalio/provider/views.py
+++ vitalio/provider/views.py
@@ -26,10 +26,10 @@ from twilio.jwt.access_token import AccessToken
 from twilio.jwt.access_token.grants import VideoGrant
 
 # Twilio Account Values
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
-api_key_sid = os.environ['TWILIO_API_KEY_SID']
-api_key_secret = os.environ['TWILIO_API_KEY_SECRET']
+account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+api_key_sid = os.environ.get('TWILIO_API_KEY_SID')
+api_key_secret = os.environ.get('TWILIO_API_KEY_SECRET')
 
 @has_role_decorator('provider')
 def provider_dashboard(request):
