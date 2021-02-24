2a95f91155571b88395f17135073471e142415b2
Obakeng <obakeng@staffdeals.co.za>
2021-02-24 18:01:45 +0200

    Removed Docker, Nginx, and Postgres files.

--- vitalio/manage.py
+++ vitalio/manage.py
@@ -3,9 +3,9 @@
 import os
 import sys
 
-
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vitalio.settings')
+     
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
