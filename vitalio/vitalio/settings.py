d55f650cd9586d2e365f45951d3d6531c67400b7
Obakeng <mosadiobakeng7@gmail.com>
2021-03-02 14:06:54 +0200

    Changed to production database.

--- vitalio/vitalio/settings.py
+++ vitalio/vitalio/settings.py
@@ -78,21 +78,21 @@ WSGI_APPLICATION = 'vitalio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 # DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "vitalio_prod",
-#         "USER": "dbadmin",
-#         "PASSWORD": "vitalio@db2021!",
-#         "HOST": "localhost",
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "vitalio_prod",
+        "USER": "dbadmin",
+        "PASSWORD": "vitalio@db2021!",
+        "HOST": "localhost",
+    }
+}
 
 
 # Password validation
