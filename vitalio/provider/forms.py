b5268c2db760b9a818e4ab56686f73277bb66d0a
Obakeng <mosadiobakeng7@gmail.com>
2021-03-30 13:45:48 +0200

    Added date validation which checks if user is entering a day before the current day.

--- vitalio/provider/forms.py
+++ vitalio/provider/forms.py
@@ -1,5 +1,7 @@
 # Django
 from django.forms import ModelForm, DateInput, TimeInput
+from datetime import date, datetime
+from django.core.exceptions import ValidationError
 
 # Models
 from .models import Schedule, Booking
@@ -22,6 +24,24 @@ class ScheduleForm(ModelForm):
     self.fields['day'].input_formats = ('%Y-%m-%d',)
     self.fields['start_time'].input_formats = ('%H:%M',)
     self.fields['end_time'].input_formats = ('%H:%M',)
+
+  def clean_day(self):
+    day = self.cleaned_data["day"]
+
+    if date.today() > day:
+      raise ValidationError("Date cannot be in the past.")
+
+    return day
+
+  def clean_end_time(self):
+
+    start_time = self.cleaned_data["start_time"]
+    end_time = self.cleaned_data["end_time"]
+
+    if start_time > end_time:
+      raise ValidationError("Your end time cannot be before your start time.")
+
+    return end_time
 
 class BookingForm(ModelForm):
   class Meta:
