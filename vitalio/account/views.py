3dd6356ae8fb7468beb7ab323b2c0fb10ad09fa2
Obakeng <mosadiobakeng7@gmail.com>
2021-04-08 17:57:03 +0200

    Fixed domain value on reset email

--- vitalio/account/views.py
+++ vitalio/account/views.py
@@ -208,12 +208,12 @@ def password_reset_request(request):
 					email_template_name = "password/password_reset_email.txt"
 					c = {
 					"email":user.email,
-					'domain':'127.0.0.1:8000',
+					'domain':'vitalio.co',
 					'site_name': 'Vitalio',
 					"uid": urlsafe_base64_encode(force_bytes(user.pk)),
 					"user": user,
 					'token': default_token_generator.make_token(user),
-					'protocol': 'http',
+					'protocol': 'https',
 					}
 					email = render_to_string(email_template_name, c)
                     
