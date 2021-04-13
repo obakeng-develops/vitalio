ca29a07f898ddcb2f36d9206405cbda356836370
Obakeng <mosadiobakeng7@gmail.com>
2021-04-13 15:24:04 +0200

    Changed WSGI config.

--- vitalio/vitalio/wsgi.py
+++ vitalio/vitalio/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vitalio.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vitalio.settings')
 
 application = get_wsgi_application()
